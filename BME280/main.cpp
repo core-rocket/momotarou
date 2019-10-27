e12ec751698409857e29f48af46b433def6b55ea
tubemerc <tubemerc+1@gmail.com>
2019-10-27 17:16:05 +0900

    [update]main.cpp

--- BME280/main.cpp
+++ BME280/main.cpp
@@ -14,15 +14,12 @@ Serial pc(PA_9, PA_10, 9600); //pin19,20 TX,RX
 
 float pre, tem;
 
-float a =10.0;
-
 int main(){
     wait(1.0); //気持ち
     bme.initialize();
     pc.printf("Start.\n\r");
     myled = 1;
     while(1){
-pc.printf("%f", a);
         pc.printf("Pre: %f\n\r", bme.getPressure());
         pc.printf("Tem: %f\n\r", bme.getTemperature());
         wait(0.2);
