20c52e89fc0892c6ac46ac7ff49549264c9c2e46
tubemerc <tubemerc+1@gmail.com>
2019-10-27 17:08:06 +0900

    Initial commit

--- BME280/main.cpp
+++ BME280/main.cpp
@@ -7,12 +7,14 @@ SDO : LOW
 #include "mbed.h"
 #include "BME280.h"
 
-DigitalOut myled(PB_1);
-Serial pc(PA_9, PA_10, 115200); //pin 19, 20
+BME280 bme = BME280(PB_7, PB_6); //pin30,29 SDA,SCL
+
+DigitalOut myled(PB_1); //pin15
+Serial pc(PA_9, PA_10, 9600); //pin19,20 TX,RX
 
 float pre, tem;
 
-BME280 bme = BME280(PB_7, PB_6); //pin 30, 29
+float a =10.0;
 
 int main(){
     wait(1.0); //気持ち
@@ -20,7 +22,9 @@ int main(){
     pc.printf("Start.\n\r");
     myled = 1;
     while(1){
+pc.printf("%f", a);
         pc.printf("Pre: %f\n\r", bme.getPressure());
         pc.printf("Tem: %f\n\r", bme.getTemperature());
+        wait(0.2);
     }
 }
