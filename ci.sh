694eeef1a3d854ae310739a12c7314e021394bbe
sksat <sksat@sksat.net>
2019-12-08 18:29:52 +0900

    [add] build script

---
+++ ci.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+code=0
+onFailure(){
+	code=$?
+}
+trap onFailure ERR
+
+./build.sh 24LC1025
+./build.sh ADXL375
+./build.sh BME280
+./build.sh GYSFDMAXB
+./build.sh HX711
+./build.sh MPU9250
+./build.sh SDCard
+
+exit $code
