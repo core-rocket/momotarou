54cc685d7374e4a180e744953c03d012ae9ca8a6
sksat <sksat@sksat.net>
2019-12-08 19:26:20 +0900

    [update] build with parallel

--- ci.sh
+++ ci.sh
@@ -5,12 +5,8 @@ onFailure(){
 }
 trap onFailure ERR
 
+parallel ./build.sh {} ::: ADXL375 BME280 GYSFDMAXB HX711 MPU9250 SDCard
+
 #./build.sh 24LC1025
-./build.sh ADXL375
-./build.sh BME280
-./build.sh GYSFDMAXB
-./build.sh HX711
-./build.sh MPU9250
-./build.sh SDCard
 
 exit $code
