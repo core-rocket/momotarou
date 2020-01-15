9c2217faabd1f8fda5ce1f5c4066ce579559d695
sksat <sksat@sksat.net>
2020-01-15 20:27:48 +0900

    [add] send data test by TWE-Lite.hpp

--- TWE-Lite/main.cpp
+++ TWE-Lite/main.cpp
@@ -1,7 +1,15 @@
 #include "mbed.h"
+#include "TWE-Lite/TWE-Lite.hpp"
 
-Serial pc(PA_9, PA_10, 9600); //pin19,20 TX,RX
+Serial pc(PA_2, PA_3, 115200); //pin8,9 TX,RX
 
 int main(){
     pc.printf("start TWE-Lite.\n\r");
+
+	TWE_Lite twe(PA_9, PA_10, 115200);
+
+	twe.init();
+	twe.send_simple(0x00, 0x00, "hoge");
+	const auto ret = twe.check_send();
+	pc.printf("ret: %d\n\r", ret);
 }
