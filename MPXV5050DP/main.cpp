5a5e43a9b51b0dccf72c0c81ef8c9f61831cb2cd
tubemerc <54838270+tubemerc@users.noreply.github.com>
2020-01-20 22:03:39 +0900

    [add]sending to CANbus

--- MPXV5050DP/main.cpp
+++ MPXV5050DP/main.cpp
@@ -1,9 +1,29 @@
 #include "mbed.h"
 AnalogIn analogin(PA_0); //pin6
 Serial pc(PA_9, PA_10, 9600); //pin19,20 TX,RX
-DigitalOut myled(PB_1); //pin15
+CAN can(PA_11, PA_12); //pin21,22 rd,td
+
 float voltage=0, kpa=0, sensorvalue, velocity;
 float air_density=1.1834, offset=0.2, coefficient=1.0;
+char senddata[5];
+
+union Float2Byte{
+    float _float;
+    char _byte[4];
+};
+typedef union Float2Byte Float2Byte;
+
+void send(float value, char moji){
+    senddata[4] = moji;
+    Float2Byte sendFloat;
+    sendFloat._float = value;
+    for(int i=0;i<4;++i){
+        senddata[i] = sendFloat._byte[i];
+    }
+    if(can.write(CANMessage(0x01, senddata, 5))){ //ID:0x01
+        pc.printf("Send.\n\r");
+    } 
+}
 
 int main(){
     pc.printf("Start.\n\r");
@@ -15,5 +35,7 @@ int main(){
         pc.printf("sensorvalue: %f\n\r", sensorvalue);
         pc.printf("Pressure: %f\n\r", kpa);
         pc.printf("IAS: %f\n\r", velocity);
+        send(kpa, 'k');
+        send(velocity, 'v');
     }
 }
