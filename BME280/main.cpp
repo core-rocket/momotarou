8c73ba28f0afb2370b20a12f94f9a92b3e76909c
tubemerc <54838270+tubemerc@users.noreply.github.com>
2020-01-20 22:44:48 +0900

    [add]sending to CANbus

--- BME280/main.cpp
+++ BME280/main.cpp
@@ -6,22 +6,42 @@ SDO : LOW
 */
 #include "mbed.h"
 #include "BME280.h"
-
 BME280 bme = BME280(PB_7, PB_6); //pin30,29 SDA,SCL
-
-DigitalOut myled(PB_1); //pin15
 Serial pc(PA_9, PA_10, 9600); //pin19,20 TX,RX
+CAN can(PA_11, PA_12); //pin21,22 rd,td
 
 float pre, tem;
+char senddata[5];
+
+union Float2Byte{
+    float _float;
+    char _byte[4];
+};
+typedef union Float2Byte Float2Byte;
+
+void send(float value, char moji){
+    senddata[4] = moji;
+    Float2Byte sendFloat;
+    sendFloat._float = value;
+    for(int i=0;i<4;++i){
+        senddata[i] = sendFloat._byte[i];
+    }
+    if(can.write(CANMessage(0x02, senddata, 5))){ //ID:0x02
+        pc.printf("Send.\n\r");
+    } 
+}
 
 int main(){
-    wait(1.0); //気持ち
+    wait(0.1);
     bme.initialize();
     pc.printf("Start.\n\r");
-    myled = 1;
     while(1){
-        pc.printf("Pre: %f\n\r", bme.getPressure());
-        pc.printf("Tem: %f\n\r", bme.getTemperature());
+        pre = bme.getPressure();
+        tem = bme.getTemperature();
+        pc.printf("Pre: %f\n\r", pre);
+        pc.printf("Tem: %f\n\r", tem);
         wait(0.2);
+        send(pre, 'p');
+        send(tem, 't');
     }
 }
