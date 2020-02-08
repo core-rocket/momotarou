1f08b1f763099a93763b80e55a6325ebb126a723
tubemerc <tubemerc+1@gmail.com>
2020-02-09 05:11:39 +0900

    [fix]CANsend&read

--- CANbus/master/main.cpp
+++ CANbus/master/main.cpp
@@ -1,63 +1,32 @@
-/*
-CAN通信master側
-2種類のIDで送信する．
-rd - RXD
-td - TXD
-MCP2551まわりは以下参照(終端Rは410ohmを使った，そこにあったので)
-https://raw.githubusercontent.com/rummanwaqar/teensy_can/master/schematic.png
-*/
 #include "mbed.h"
-Serial pc(PA_9, PA_10, 9600); //pin19,20 TX,RX
-CAN can(PA_11, PA_12); //pin21,22 rd,td
 
-char senddata_1[5];
-char senddata_3[13];
+Serial pc(PA_9, PA_10, 115200); //pin19,20 TX,RX
+CAN can(PA_11, PA_12, 1000000); //pin21,22 rd,td
+
+char senddata[5];
 
 union Float2Byte{
     float _float;
     char _byte[4];
-};
-typedef union Float2Byte Float2Byte;
+}f2b;
 
-void send_1(float value, char moji){
-    senddata_1[4] = moji;
-    Float2Byte sendFloat;
-    sendFloat._float = value;
-    for(int i=0;i<4;++i){
-        senddata_1[i] = sendFloat._byte[i];
+void send(int id, float value, char moji){
+    senddata[0] = moji;
+    f2b._float = value;
+    for(int i=1;i<5;++i){
+        senddata[i] = f2b._byte[i];
     }
-    if(can.write(CANMessage(0x02, senddata_1, 5))){ //ID:0x02
-        pc.printf("Send.\n\r");
-    } 
-}
-
-void send_3(float value_1,float value_2, float value_3, char moji){
-    pc.printf("send_pre.");
-    senddata_3[12] = moji;
-    Float2Byte sendFloat;
-    sendFloat._float = value_1;
-    for(int i=0;i<4;++i){
-        senddata_3[i] = sendFloat._byte[i];
-    }
-    sendFloat._float = value_2;
-    for(int i=4;i<8;++i){
-        senddata_3[i] = sendFloat._byte[i];
-    }
-    sendFloat._float = value_3;
-    for(int i=8;i<12;++i){
-        senddata_3[i] = sendFloat._byte[i];
-    }
-    if(can.write(CANMessage(0x05, senddata_3, 13))){ //ID:0x05
-        pc.printf("Send.\n\r");
+    CANMessage msg(id, senddata, 5);
+    if(can.write(msg)){
+        pc.printf("%d\n\r", msg.len);
     } 
 }
 
 int main(){
-    pc.printf("start.\n\r");
+    wait(0.1);
+    pc.printf("Start.\n\r");
     while(1){
+        send(0x01, 1.0, 'a');
         wait(0.1);
-        send_1(1.0, 'a');
-        wait(0.1);
-        send_3(2.0, 3.0, 4.0, 'b');
     }
 }
--- CANbus/slave/main.cpp
+++ CANbus/slave/main.cpp
@@ -1,73 +1,28 @@
-/*
-CAN通信slave側
-rd - RXD
-td - TXD
-*/
 #include "mbed.h"
 
-//Serial pc(PA_9, PA_10, 9600); //pin19,20 TX,RX
-Serial pc(PA_2, PA_3, 9600); //pin8,9 TX,RX
+Serial pc(PA_9, PA_10, 115200); //pin19,20 TX,RX
+//Serial pc(PA_2, PA_3, 115200); //pin8,9 TX,RX
+CAN can(PA_11, PA_12, 1000000); //pin21,22 rd,td
 
-CAN can(PA_11, PA_12); //pin21,22 rd,td
-
-float _x, _y, _z;
 CANMessage msg;
 
 union Float2Byte{
     float _float;
     char _byte[4];
-};
-typedef union Float2Byte Float2Byte;
-
-void receive(){
-    Float2Byte getFloat;
-    if(can.read(msg)){
-        if(msg.id == 0x01){
-            for(int i=0;i<4;++i){
-                getFloat._byte[i] = msg.data[i];
-            }            
-            pc.printf("ID:%d,%c,%f\n\r", msg.id, msg.data[4], getFloat._float);
-        }
-        if(msg.id == 0x02){
-            for(int i=0;i<4;++i){
-                getFloat._byte[i] = msg.data[i];
-            }            
-            pc.printf("ID:%d,%c,%f\n\r", msg.id, msg.data[4], getFloat._float);
-        }
-        if(msg.id == 0x03){
-            for(int i=0;i<4;++i){
-                getFloat._byte[i] = msg.data[i];
-            }            
-            pc.printf("ID:%d,%c,%f\n\r", msg.id, msg.data[4], getFloat._float);
-        }
-        if(msg.id == 0x04){
-            for(int i=0;i<4;++i){
-                getFloat._byte[i] = msg.data[i];
-            }            
-            pc.printf("ID:%d,%c,%f\n\r", msg.id, msg.data[4], getFloat._float);
-        }
-        if(msg.id == 0x05){
-            for(int i=0;i<4;++i){
-                getFloat._byte[i] = msg.data[i];
-            }
-            _x = getFloat._float;
-            for(int i=4;i<8;++i){
-                getFloat._byte[i] = msg.data[i];
-            }
-            _y = getFloat._float;
-            for(int i=8;i<12;++i){
-                getFloat._byte[i] = msg.data[i];
-            }
-            _z = getFloat._float;
-            pc.printf("ID:%d,%c,%f,%f,%f\n\r", msg.id, msg.data[12], _x, _y, _z);
-        }
-    }
-}
+}f2b;
 
 int main(){
+    wait(0.1);
     pc.printf("Start.\n\r");
-    can.attach(receive, CAN::RxIrq);
     while(1){
-        wait(0.2);
+        if(can.read(msg)){
+            //if(msg.id == 10){
+                for(int i=1;i<5;++i){
+                    f2b._byte[i] = msg.data[i];
+                }
+                pc.printf("ID:%d,%c,%f\n\r", msg.id, msg.data[0], f2b._float);
+            //}
+        }
+        //wait(0.1);
     }
 }
