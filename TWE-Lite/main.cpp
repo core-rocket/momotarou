bbd1fb84ce5e97564eb6c4568a658b6660467af0
sksat <sksat@sksat.net>
2020-02-08 14:37:47 +0900

    [add] send test

--- TWE-Lite/main.cpp
+++ TWE-Lite/main.cpp
@@ -9,7 +9,18 @@ int main(){
 	TWE_Lite twe(PA_9, PA_10, 115200);
 
 	twe.init();
-	twe.send_simple(0x00, 0x00, "hoge");
-	const auto ret = twe.check_send();
-	pc.printf("ret: %d\n\r", ret);
+
+	char msgbuf[250];
+	for(int i=0;i<250;i++){
+		msgbuf[i] = 'T';
+	}
+
+	while(true){
+		for(int len=1;len<16;len++){
+			twe.send_buf_extend(0x00, 0x00, msgbuf, len);
+			const auto ret = twe.check_send();
+			pc.printf("ret: %d\n\r", ret);
+			wait_ms(1000);
+		}
+	}
 }
