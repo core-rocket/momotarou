d3db3591c43eb4418eed8d83701ab9f266808c9d
sksat <sksat@sksat.net>
2020-02-09 21:39:17 +0900

    [add] CAN message id

--- common.h
+++ common.h
@@ -1,5 +1,5 @@
-#ifndef PHASE_HPP_
-#define PHASE_HPP_
+#ifndef COMMON_HPP_
+#define COMMON_HPP_
 
 enum class Phase {
 	standby,
@@ -7,6 +7,20 @@ enum class Phase {
 	burning,
 	rising,
 	parachute,
+};
+
+enum class MsgID : uint8_t {
+	command		= 0x01,
+	error,
+	air_press,
+	acc,
+	gps,
+	phase,
+	angular_vel,
+	burn_press,
+	dynamic_press,
+	temperature,
+	magnetic,
 };
 
 #endif
