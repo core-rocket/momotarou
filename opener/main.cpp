2b0fe49fea0836f2deec030b6d5eda9eb163811a
sksat <sksat@sksat.net>
2020-02-09 17:36:11 +0900

    [add] phase

--- opener/main.cpp
+++ opener/main.cpp
@@ -1,7 +1,44 @@
 #include "mbed.h"
+#include "../phase.h"
 
 Serial pc(PA_2, PA_3, 115200); //pin8,9 TX,RX
+CAN can(PA_11, PA_12);
+
+namespace global {
+	Phase phase;
+	CANMessage can_msg;
+}
+
+void can_recv();
 
 int main(){
-    pc.printf("start TWE-Lite.\n\r");
+	global::phase = Phase::standby;
+    pc.printf("start opener.\n\r");
+
+	can.attach(can_recv, CAN::RxIrq);
+
+	switch(global::phase){
+	case Phase::standby:
+		// 待機フェーズ: 無線でフライトモードON
+		break;
+	case Phase::flight:
+		// 離床判定
+		break;
+	case Phase::burning:
+		// 加速上昇中
+		break;
+	case Phase::rising:
+		// 慣性飛行: 離床判定
+		break;
+	case Phase::parachute:
+		break;
+	}
+}
+
+void can_recv(){
+	if(can.read(global::can_msg) == 0)
+		return;
+
+	const &msg = global::can_msg;
+	// 簡単なparse
 }
---
+++ phase.h
@@ -0,0 +1,12 @@
+#ifndef PHASE_HPP_
+#define PHASE_HPP_
+
+enum class Phase {
+	standby,
+	flight,
+	burning,
+	rising,
+	parachute,
+};
+
+#endif
