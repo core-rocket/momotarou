8f2ab18e31f41320951cafc455f2be4674ef2a0a
tubemerc <tubemerc+1@gmail.com>
2020-02-12 19:02:27 +0900

    [updata] CANsend [change] coefficient

--- MPXV5050DP/main.cpp
+++ MPXV5050DP/main.cpp
@@ -1,41 +1,50 @@
 #include "mbed.h"
+/*
+Vout 0.2~4.7[V]
+analogIn 0.133~3.133[V]
+coefficient:50/(3.133-0.133)=16.666
+*/
 AnalogIn analogin(PA_0); //pin6
-Serial pc(PA_9, PA_10, 9600); //pin19,20 TX,RX
+Serial pc(PA_9, PA_10, 115200); //pin19,20 TX,RX
 CAN can(PA_11, PA_12); //pin21,22 rd,td
 
-float voltage=0, kpa=0, sensorvalue, velocity;
-float air_density=1.1834, offset=0.2, coefficient=1.0;
+float kpa = 0.0;
+float sensorvalue = 0.0;
+float offset = 0.0;
+float coefficient = 16.666;
 char senddata[5];
 
 union Float2Byte{
     float _float;
     char _byte[4];
-};
-typedef union Float2Byte Float2Byte;
+}f2b;
 
-void send(float value, char moji){
-    senddata[4] = moji;
-    Float2Byte sendFloat;
-    sendFloat._float = value;
-    for(int i=0;i<4;++i){
-        senddata[i] = sendFloat._byte[i];
+void send(int id, float value, char moji){
+    senddata[0] = moji;
+    f2b._float = value;
+    for(int i=1;i<5;++i){
+        senddata[i] = f2b._byte[i];
     }
-    if(can.write(CANMessage(0x01, senddata, 5))){ //ID:0x01
-        pc.printf("Send.\n\r");
-    } 
+    CANMessage msg(id, senddata, 5);
+    if(can.write(msg)){
+        pc.printf("%d,%c\n\r", id, moji);
+    }
 }
 
 int main(){
     pc.printf("Start.\n\r");
+    wait(0.1);
     while(1){
-        wait(0.1);
         sensorvalue = analogin.read()*3.3f - offset;
-        kpa = sensorvalue*10.0f; //10=50(kPa)/5(V)
-        velocity = coefficient*sqrt(2.0f*kpa*1000/air_density);
+        if(sensorvalue > 0.13){
+            kpa = sensorvalue * coefficient;
+        }
+        else{
+            kpa = 0.0;
+        }
+        
         pc.printf("sensorvalue: %f\n\r", sensorvalue);
         pc.printf("Pressure: %f\n\r", kpa);
-        pc.printf("IAS: %f\n\r", velocity);
-        send(kpa, 'k');
-        send(velocity, 'v');
+        send(0x09, kpa, 'k');
     }
 }
