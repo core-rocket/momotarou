54d2d943a1b68cf9b09303f3497813ab28e78025
sksat <sksat@sksat.net>
2020-02-13 17:57:17 +0900

    [add] acc check

--- opener/main.cpp
+++ opener/main.cpp
@@ -10,6 +10,7 @@ namespace global {
 	Phase phase;
 	CANMessage recv_msg;
 	float apress;
+	uint8_t accnum;
 	uint8_t downnum;
 }
 
@@ -38,7 +39,7 @@ int main(){
 		if(!can.write(msg))
 			pc.printf("error: cannot send phase\r\n");
 
-		pc.printf("phase: %d, apress: %f, downnum: %d\r\n", (int)static_cast<uint8_t>(phase), global::apress, (int)global::downnum);
+		pc.printf("phase: %d, accnum: %d, apress: %f, downnum: %d\r\n", (int)static_cast<uint8_t>(phase), (int)global::accnum, global::apress, (int)global::downnum);
 
 		switch(phase){
 		case Phase::standby:
@@ -46,7 +47,7 @@ int main(){
 			break;
 		case Phase::flight:
 			// 離床判定
-			if(!flight_pin)
+			if(!flight_pin && global::accnum >= 5)
 				global::phase = Phase::burning;
 			break;
 		case Phase::burning:
@@ -54,7 +55,7 @@ int main(){
 			break;
 		case Phase::rising:
 			// 慣性飛行: 開放判定
-			if(global::downnum > 10)
+			if(global::downnum >= 10)
 				global::phase = Phase::parachute;
 			break;
 		case Phase::parachute:
@@ -93,6 +94,13 @@ void can_recv(){
 		break;
 	case MsgID::acc:
 		// push to FIFO
+		{
+			const Float2Byte *acc = (Float2Byte*)msg.data;
+			if(acc->_float > 1.1)
+				global::accnum++;
+			else
+				global::accnum = 0;
+		}
 		break;
 	case MsgID::air_press:
 		// push to FIFO
