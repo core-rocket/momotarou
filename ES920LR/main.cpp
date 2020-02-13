a7771eefe0bd32d758606e2802d0d740e7678e95
sksat <sksat@sksat.net>
2020-02-13 15:48:04 +0900

    [add] flight mode on by ES920LR

--- ES920LR/main.cpp
+++ ES920LR/main.cpp
@@ -2,41 +2,50 @@
 
 Serial pc(PA_2, PA_3, 115200); //pin8,9 TX,RX
 Serial es920lr(PA_9, PA_10, 115200);
+CAN can(PA_11, PA_12); //pin21,22 rd,td
 
-uint8_t buf[50];
-
-bool recv(){
+uint8_t recv(char *buf){
 	const auto len = es920lr.getc();
 	if(len < 0)
-		return false;
+		return 0;
 	for(uint8_t i=0;i<len;i++){
 		if(len > 50)
 			break;
 		buf[i] = es920lr.getc();
 	}
-
-	return true;
+	return len;
 }
 
-
+int check_response(const uint8_t &len, const char *buf){
+	if(len != 6)
+		return -1;
+	if(buf[0] == 'N' && buf[1] == 'G')
+		return -1;
+	if(buf[0] == 'O' && buf[1] == 'K')
+		return 1;
+}
 
 int main(){
-    pc.printf("start TWE-Lite.\n\r");
+	char buf[100];
+    pc.printf("start ES920LR.\n\r");
 
 	wait(5.0);
 
-	es920lr.printf("start\r\n");
+	es920lr.putc(0x01);
+	es920lr.putc('A');
+	while(!recv(buf));
+	wait_ms(250);
 
 	while(true){
-		if(recv()){
-			uint8_t len = 50;
-			es920lr.putc(len);
-			for(uint8_t i=0;i<len;i++){
-				es920lr.putc(buf[i]);
-			}
-			while(!recv());
-			if(buf[0] == 'O' && buf[1] == 'K')
-				wait_ms(250);
+		const auto len = recv(buf);
+		if(len == 0) continue;
+
+		if(len == 2 && buf[0] == 'F' && buf[1] == 'L'){
+			can.write(CANMessage(0x01, buf, 1));
+			es920lr.putc(0x11);
+			es920lr.printf("flight mode on...");
+			while(!recv(buf));
+			wait_ms(250);
 		}
 	}
 }
