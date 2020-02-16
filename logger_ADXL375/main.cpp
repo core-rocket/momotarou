7cead348192dd86e24a7a6047b26ba3b9f5ca513
haru-11 <degmyb.5534g@gmail.com>
2020-02-16 14:46:21 +0900

    [update]文字で保存

--- logger_ADXL375/main.cpp
+++ logger_ADXL375/main.cpp
@@ -11,27 +11,50 @@ MOSI:pin28 --- pin3 CMD
 #include "ADXL375_i2c.h"
 
 DigitalOut myled(PB_1); //pin15
+ADXL375_i2c adxl = ADXL375_i2c(PB_7, PB_6);
 Serial pc(PA_9, PA_10, 9600); //pin19,20 TX,RX
 
 SDFileSystem sd = SDFileSystem(PB_5, PB_4, PB_3, PA_15, "sd"); //pin28,27,26,25
+
+int16_t acc[3];
+uint8_t whoami;
 
 int main(){
     wait(1.0); //気持ち
     pc.printf("Start.\n\r");
 
-    FILE *fp = fopen("/sd/test.csv", "w");
-    //FILE *fp = fopen("/sd/test.txt", "w");
-    if(fp != NULL){
+    whoami = adxl.readByte(ADXL375_I2C_HIGH_READ, ADXL375_DEVID_REG);
+    pc.printf("I AM 0x%x\n\r", whoami); //0xE5で正常
+
+    if (whoami == 0xE5){ 
+        pc.printf("ADXL375 is detected.\n\r");
+        wait(1.0); //気持ち
+        adxl.initADXL375();
         myled = 1;
-        pc.printf("Writing to SDcard......\n\r");
-        for(int i=1;i<10;i++){
-            fprintf(fp, "%d\n", i);
+        wait(1.0); //気持ち
+   }
+   else{
+        pc.printf("Could not detect ADXL375.\n\r");
+        while(1);
+    }
+
+    while(1) {  
+        FILE *fp = fopen("/sd/test.csv", "a");
+
+        if(fp != NULL){
+            myled = 1;
+            adxl.readAccelData(acc);
+            //pc.printf("Acc: %d, %d, %d\n\r", acc[0], acc[1], acc[2]);
+            pc.printf("Acc:%f,%f,%f\r\n",(float)acc[0]/20.5, (float)acc[1]/20.5, (float)acc[2]/20.5); 
+            fprintf(fp, "%f,%f,%f\r\n",(float)acc[0]/20.5, (float)acc[1]/20.5, (float)acc[2]/20.5);
+            fclose(fp);
+            myled = 0;
+        }else{
+            pc.printf("Failed.\n\r");
         }
-        fclose(fp);
-        myled = 0;
+    
+    wait(0.5);
+
     }
-    else{
-        pc.printf("Failed.\n\r");
-    }
-    pc.printf("End.\n\r");
+
 }
