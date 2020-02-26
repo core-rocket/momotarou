9667d866e4c72b9ec2ae8d7daa640ae6293899d6
sksat <sksat@sksat.net>
2020-02-26 18:27:22 +0900

    [add] recieve CAN message

--- TWE-Lite/main.cpp
+++ TWE-Lite/main.cpp
@@ -1,14 +1,24 @@
 #include "mbed.h"
+#include "../common.h"
+#include "../telemetry.hpp"
+#include "../utility.hpp"
 #include "TWE-Lite/TWE-Lite.hpp"
 
-Serial pc(PA_2, PA_3, 115200); //pin8,9 TX,RX
+Serial pc(PA_2, PA_3, BRATE); //pin8,9 TX,RX
+CAN can(PA_11, PA_12, CAN_SPEED);
+
+namespace global {
+	CANMessage can_msg;
+}
+
+void can_recv();
 
 int main(){
-    pc.printf("start TWE-Lite.\n\r");
-
 	TWE_Lite twe(PA_9, PA_10, 115200);
 
+	pc.printf("start TWE-Lite.\n\r");
 	twe.init();
+	can.attach(can_recv, CAN::RxIrq);
 
 	char msgbuf[250];
 	for(int i=0;i<250;i++){
@@ -22,5 +32,22 @@ int main(){
 			pc.printf("ret: %d\n\r", ret);
 			wait_ms(1000);
 		}
+	}
+}
+
+void can_recv(){
+	using namespace telemetry;
+	using namespace utility::can;
+
+	if(can.read(global::can_msg) == 0)
+		return;
+
+	const auto &msg = global::can_msg;
+
+	switch(static_cast<MsgID>(msg.id)){
+	case MsgID::air_press:
+		break;
+	default:
+		break;
 	}
 }
