bdabc7b7ea9eb7d1aee45e18ac27fb2e9c6ef38e
sksat <sksat@sksat.net>
2020-02-26 18:53:37 +0900

    [add] queue library

--- utility.hpp
+++ utility.hpp
@@ -13,6 +13,38 @@ namespace utility {
 			return reinterpret_cast<const T*>(ptr);
 		}
 	}
+
+	template<typename T, size_t BUFSIZE>
+	class queue {
+	public:
+		queue() : siz(0), read(0), write(0) {}
+
+		auto empty() const -> bool { return (size() == 0); }
+		auto size() const -> size_t { return this->siz; }
+
+		auto front() const -> const T& { return buf[read]; }
+
+		auto push(const T &val) -> void {
+			buf[write] = val;
+			siz++;
+			write++;
+			if(write == BUFSIZE)
+				write = 0;
+		}
+
+		auto pop() -> T {
+			const auto &ret = buf[read];
+			siz--;
+			read++;
+			if(read == BUFSIZE)
+				read = 0;
+			return ret;
+		}
+	protected:
+		size_t siz;
+		size_t read, write;
+		T buf[BUFSIZE];
+	};
 }
 
 #endif
