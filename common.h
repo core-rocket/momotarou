664ba2f2c6921162067ac242410aabcd9b6b7f84
tubemerc <tubemerc+1@gmail.com>
2020-03-25 17:50:55 +0900

    [update] MsgID

--- common.h
+++ common.h
@@ -14,18 +14,18 @@ enum class Phase : uint8_t {
 
 enum class MsgID : unsigned int {	// 29bit identifier
 	command		= 0x01,
-	error,
-	air_press,
-	acc_norm,
-	gps,
-	phase,
-	quaternion,
-	acc,
-	gyro,
+	error = 0x02,
+	air_press = 0x03,
+	acc_norm = 0x04,
+	gps = 0x05,
+	phase = 0x06,
+	quaternion = 0x07,
+	acc = 0x08,
+	gyro = 0x09,
 	burn_press = 0x0A,
-	dynamic_press,
-	temperature,
-	magnetic,
+	dynamic_press = 0x0B,
+	temperature = 0x0C,
+	magnetic = 0x0D,
 };
 
 #endif
